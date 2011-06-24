c8e3153037539a551ef084b269d8d97dce46d497
zhili <modus_li@163.com>
2011-06-25 01:29:33 +1000

    completed user signup

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -44,7 +44,53 @@ describe UsersController do
       get :show, :id=>@user
       response.should have_selector("h1>img",:class=>"gravatar")
     end
+  end
+  
+  describe "post 'create'" do
     
+    describe "failure" do
+      before(:each) do
+        @attr={:name=>"",:email=>"",:password=>"",:password_confirmation=>""}
+      end
+    
+      it "should not create a new user" do
+        lambda do
+          post :create, :user=>@attr
+        end.should_not change(User, :count)
+      end
+    
+      it "should have a right title" do
+        post :create, :user=>@attr
+        response.should have_selector("title",:content=>"Sign up")
+      end
+    
+      it "should render the 'new' page" do
+        post :create, :user=>@attr
+        response.should render_template('new')
+      end
+    end
+    
+    describe "success" do
+      before(:each) do
+        @attr={:name=>"zhili",:email=>"zhili@163.com",:password=>"password",:password_confirmation=>"password"}
+      end
+      
+      it "should create a user" do
+        lambda do
+          post :create, :user=>@attr
+        end.should change(User,:count).by(1)
+      end
+      
+      it "should redirect to user show page" do
+        post :create, :user=>@attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+      
+      it "should have a welcome message" do
+        post :create, :user=>@attr
+        flash[:success].should =~ /welcome to Ray's micropost/i
+      end
+    end
   end
 
 end
